7aa5ce57f70673efd878642124686ed02366eca8
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2024-09-03 13:31:50 +0200

    [UPDATE]

--- calculator/win.go
+++ calculator/win.go
@@ -8,7 +8,6 @@ import (
 	"gambler/backend/tools"
 	"math"
 
-	"github.com/gofiber/fiber/v2/log"
 	logger "github.com/gofiber/fiber/v2/log"
 )
 
@@ -30,15 +29,7 @@ func generateBetLog(bets []models.UserBet) []BetLog {
 	return betLogs
 }
 
-func generateTotalBet(userBets []models.UserBet) map[string]float64 {
-	total := make(map[string]float64)
-	for _, bet := range userBets {
-		total[bet.BetOption] += bet.Amount
-	}
-	return total
-}
-
-func CalculateWinningAmount(betID string, inputIndex int, betLog []BetLog) (float64, int) {
+func CalculateWinningAmount(betID string, userID uint, inputIndex int, betLog []BetLog) (float64, int) {
 	bet, err := handlers.Cache.GetBetById(betID)
 	if err != -1 {
 		return 0, err
@@ -47,26 +38,25 @@ func CalculateWinningAmount(betID string, inputIndex int, betLog []BetLog) (floa
 		return 0, tools.BET_NOT_ACTIVE
 	}
 	input := bet.BetOptions[inputIndex]
-	totalBet := generateTotalBet(bet.UserBets)
 
-	amount := 0.0
-	sumBet := 0.0
-	otherWin := 0.0
+	amount := 0.0   // Total bet amount
+	sumBet := 0.0   // My total bet in that option
+	otherWin := 0.0 // Other bets in that option
 	for _, log := range betLog {
 		logger.Info(fmt.Sprintf("%v", log))
-		if log.BetOption != input {
+		if log.BetOption == input {
 			sumBet += log.BetAmount
-		} else {
+		} else if log.BetOption != input {
 			amount += log.BetAmount
 		}
 	}
 
-	for i, bet := range totalBet {
-		log.Info(bet)
-		if i != input {
-			sumBet += bet
-		} else {
-			otherWin += bet
+	for _, bet := range bet.UserBets {
+		amount += bet.Amount
+		if bet.UserID != userID && bet.BetOption == input {
+			sumBet += bet.Amount
+		} else if bet.UserID != userID && bet.BetOption == input {
+			otherWin += bet.Amount
 		}
 	}
 
@@ -76,12 +66,53 @@ func CalculateWinningAmount(betID string, inputIndex int, betLog []BetLog) (floa
 
 	var winAmount float64
 	if otherWin == 0.0 {
-		winAmount = sumBet
+		winAmount = amount
 	} else {
-		winAmount = sumBet * (amount / otherWin)
+		winAmount = amount * (sumBet / otherWin)
 	}
 
-	winPercentage := math.Trunc(winAmount/amount*100) / 100
+	winPercentage := math.Trunc(winAmount/sumBet*100) / 100
+
+	fmt.Println("Winning percentage: ", winPercentage, winAmount, amount, otherWin, sumBet)
+
+	return winPercentage, -1
+}
+
+func CalculateWinForExistedBet(betID string, userID uint, inputIndex int) (float64, int) {
+	bet, err := handlers.Cache.GetBetById(betID)
+	if err != -1 {
+		return 0, err
+	}
+	if bet.Status != customTypes.Open {
+		return 0, tools.BET_NOT_ACTIVE
+	}
+	input := bet.BetOptions[inputIndex]
+
+	amount := 0.0   // Total Amount will win
+	sumBet := 0.0   // User Bet amount in that option
+	otherWin := 0.0 // Other's bet amount in that option
+
+	for _, bet := range bet.UserBets {
+		amount += bet.Amount
+		if bet.BetOption == input && bet.UserID != userID {
+			otherWin += bet.Amount
+		} else if bet.BetOption == input && bet.UserID == userID {
+			sumBet += bet.Amount
+		}
+	}
+
+	if sumBet == 0.0 {
+		return 0, -1
+	}
+
+	var winAmount float64
+	if otherWin == 0.0 {
+		winAmount = amount
+	} else {
+		winAmount = amount * (sumBet / otherWin)
+	}
+
+	winPercentage := math.Trunc(winAmount/sumBet*100) / 100
 
 	fmt.Println("Winning percentage: ", winPercentage, winAmount, amount, otherWin, sumBet)
 
--- routes/bets/controller/bets.go
+++ routes/bets/controller/bets.go
@@ -1,8 +1,10 @@
 package controller
 
 import (
+	"gambler/backend/handlers"
 	"gambler/backend/middleware"
 	"gambler/backend/routes/bets/service"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -11,4 +13,5 @@ func InitBetsRoute(c *fiber.App) {
 	group := c.Group("/bets", middleware.JwtGuardHandler)
 	group.Get("/", service.GetAllActiveBets)
 	group.Post("/create", service.CreateBet)
+	group.Get("/:id<int>", handlers.AddCache(time.Minute*15), service.GetBet)
 }
